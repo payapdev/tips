8e218c80b7abff03dc4cafbbd544b0ebe60c5cdb
kevinPayap <125852827+kevinPayap@users.noreply.github.com>
2023-10-18 17:00:09 +0900

    옵션 테스트

--- crawl/extractor/tiki.py
+++ crawl/extractor/tiki.py
@@ -5,7 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 
-def crawl_tiki(keyword):
+def crawl_tiki(model, option_list):
   options = Options()
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
@@ -19,59 +19,79 @@ def crawl_tiki(keyword):
   scroll_height = 800
   item_number = 1
   
-  browser.get(f"{base_url}/search?q={keyword}")
+  browser.get(f"{base_url}/search?q={model}")
   browser.implicitly_wait(100)
   soup = BeautifulSoup(browser.page_source, "html.parser")
   browser.implicitly_wait(100)
-  page_block = soup.find("div", class_="pagination-block")
-  page_list = page_block.find_all("li")
-  max_page = 1
-  # max_page = int(page_list.pop().string)
   
-  for index in range(max_page):
-      browser.get(f"{base_url}/search?q={keyword}")
-      browser.implicitly_wait(100)
-      browser.execute_script(f"window.scrollTo(0, {1 * scroll_height});")
-      time.sleep(time_interval)
-      browser.execute_script(f"window.scrollTo({1 * scroll_height}, {2 * scroll_height});")
-      time.sleep(time_interval)
-      browser.execute_script(f"window.scrollTo({2 * scroll_height}, {3 * scroll_height});")
-      time.sleep(time_interval)
-      browser.execute_script(f"window.scrollTo({3 * scroll_height}, document.body.scrollHeight);")
-      time.sleep(time_interval)
-        
-      soup = BeautifulSoup(browser.page_source, "html.parser")
-      items = soup.find_all('a', class_="product-item")
-        
-      for item in items:
-        link = item['href']
-        
-        thumbnail_part = item.find("div", class_="thumbnail")
-        thumbnail_source = thumbnail_part.find("img")
-        thumbnail = thumbnail_source['srcset'].split()[0]
-        
-        info_part = item.find("div", class_="info")
-        name_part = info_part.find("div", class_="name")
-        name = name_part.find("h3").string
-        
-        price_part = info_part.find("div", class_="price-discount__price")
-        price = int(price_part.text.strip().replace(".", "").replace("₫", "").replace(",", ""))
-        
-        product_data = {
-            "thumbnail": thumbnail.replace(",", " "),
-            "name": name.replace(",", " "),
-            "price": price,
-            "link": f"{base_url}{link}"
-        }
-        
-        results.append(product_data)
-        # browser.get(f"{base_url}{link}")
-        
-        item_number = item_number + 1
+  browser.get(f"{base_url}/search?q={model}")
+  browser.implicitly_wait(100)
+  browser.execute_script(f"window.scrollTo(0, {1 * scroll_height});")
+  time.sleep(time_interval)
+  browser.execute_script(f"window.scrollTo({1 * scroll_height}, {2 * scroll_height});")
+  time.sleep(time_interval)
+  browser.execute_script(f"window.scrollTo({2 * scroll_height}, {3 * scroll_height});")
+  time.sleep(time_interval)
+  browser.execute_script(f"window.scrollTo({3 * scroll_height}, document.body.scrollHeight);")
+  time.sleep(time_interval)
+    
+  soup = BeautifulSoup(browser.page_source, "html.parser")
+  items = soup.find_all('a', class_="product-item")
+    
+  for item in items:
+    link = item['href']
+    
+    thumbnail_part = item.find("div", class_="thumbnail")
+    thumbnail_source = thumbnail_part.find("img")
+    thumbnail = thumbnail_source['srcset'].split()[0]
+    
+    info_part = item.find("div", class_="info")
+    name_part = info_part.find("div", class_="name")
+    name = name_part.find("h3").string
+    
+    price_part = info_part.find("div", class_="price-discount__price")
+    price = int(price_part.text.strip().replace(".", "").replace("₫", "").replace(",", ""))
+    
+    option = find_matching_option(name, option_list)
+    
+    product_data = {
+        "thumbnail": thumbnail.replace(",", " "),
+        "name": name.replace(",", " "),
+        "price": price,
+        "link": f"{base_url}{link}",
+        "option": option,
+    }
+    
+    results.append(product_data)
+    # browser.get(f"{base_url}{link}")
+    
+    item_number = item_number + 1
         
   results.sort(key=lambda x: x["price"])  
   browser.close()      
   return results
+
+def find_matching_option(name, option_list):
+    # 정확한 문자열 매칭 우선
+    for option in option_list:
+        if isinstance(option, list):
+            for sub_option in option:
+                if sub_option in name:
+                    return option
+        elif option in name:
+            return option
+    
+    # 부분적으로 겹치는 문자열 예외 처리
+    for option in option_list:
+        if isinstance(option, list):
+            for sub_option in option:
+                if sub_option in name:
+                    return option
+        elif option in name:
+            return option
+    
+    # 일치하는 것이 없으면 첫 번째 옵션을 기본값으로 사용
+    return option_list[0]
   
 def crawl_options(link):
   options = Options()
@@ -106,4 +126,3 @@ def crawl_options(link):
     
   return options_wrapper
   
-crawl_options("https://tiki.vn/apple-iphone-15-pro-max-p271973414.html?spid=271973470")
--- crawl/main.py
+++ crawl/main.py
@@ -7,32 +7,61 @@ from extractor.lazada import crawl_lazada
 crawled_dataset = []
 
 # csv 파일 경로
-csv_file_path = '/Users/payap/development/tips/data/tips_data.csv'
+csv_file_path = '/Users/payap/development/tips/data/'
+file_name = 'test_option.csv'
 
 # csv 파일 불러오기
-df = pd.read_csv(csv_file_path)
+df = pd.read_csv(csv_file_path + file_name)
 search_keywords = df['search_word'].values.tolist()
-# search_keywords = [ "logitech g102", "apple pencil",]
+version = df['ver'].str.split('.').tolist()
+options = [df['option 1'].str.split('.').tolist(), df['option 2'].str.split('.').tolist()]
+print("start!")
+print(version)
+print(options)
 
-for keyword in search_keywords:
+for index, keyword in enumerate(search_keywords):
     print(keyword)
-    tiki = crawl_tiki(keyword)
-    lazada = crawl_lazada(keyword)
-    # shopee = crawl_shopee(keyword)
+    models = []
     
-    crawl_data = {
-        "keyword": keyword,
-        "tiki": tiki,
-        "lazada": lazada,
-        # "shopee": shopee
-    }
+    for ver in version[index]:
+        if ver == 'base':
+            ver = ''
+        models.append(f'{keyword} {ver}')
+    print(models)
+        
+    option_list = []
+    for option in options:
+        temp = []
+        if len(option[0]) != 0:
+            if len(option[1]) != 0:
+                for o1 in option[0]:
+                    for o2 in option[1]:
+                        temp = [o1, o2]
+            else:
+                for o1 in option[0]:
+                    temp = [o1]
+            option_list.append(temp)   
+                         
+    for model in models:
+        tiki = crawl_tiki(model, option_list)
+        # lazada = crawl_lazada(model, option_list)
+        # shopee = crawl_shopee(keyword)
+        
+        crawl_data = {
+            "keyword": model,
+            "tiki": tiki,
+            # "lazada": lazada,
+            # "shopee": shopee
+        }
+        
+        crawled_dataset.append(crawl_data)    
     
-    crawled_dataset.append(crawl_data)
+    
     
 print(crawled_dataset)
 # DataFrame 생성
 df_result = pd.DataFrame(crawled_dataset)
 
 # csv 파일로 저장
-save_path = '/Users/payap/development/tips/data/crawled_data.csv'
+save_path = '/Users/payap/development/tips/data/test_option_crawled_data.csv'
 df_result.to_csv(save_path, index=False)
